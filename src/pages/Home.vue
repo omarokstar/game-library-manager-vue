e2c816f2b412ab9f2d654a652cc3fe9b832b4697
arwaemam2 <arwaaemamm@gmail.com>
2025-04-18 22:10:37 -0700

    login&signup

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,6 @@
-<template>
+
+  /*<template>
     <div>
       <h1>Welcome to End Game Manager</h1>
     </div>
   </template>
-  
-  <script>
-  export default {
-    name: "Home",
-  };
-  </script>
